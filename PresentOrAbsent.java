70651a5261d082a721289047783d7502018a6f06
Jayakansal <21jayajindal@gmail.com>
2020-11-25 16:16:56 +0530

    this is for salary of an employee

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -15,3 +15,23 @@ if (randomNum==1) {
 	}
 	}
 }
+package com.useCase1;
+
+import java.util.Random;
+
+public class SalaryOfEmployee {
+
+        public static void main(String[] args) {
+                Random random=new Random();
+                int randomNum=random.nextInt(2);
+
+if (randomNum==1) {
+System.out.println("Employee is Present");
+int EmpHur=8;
+int ratePerHur=20;
+System.out.println("Salary:(int EmpHur* int ratePerHur)}");
+
+} else {
+System.out.println("Employee is Absent");
+System.out.println("Salary:+0");
+}
