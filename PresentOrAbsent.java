6305174e2a53a7c7de5d6d89dfcd5e79048bf432
Jayakansal <21jayajindal@gmail.com>
2020-11-25 18:44:49 +0530

    This is for employee wages

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -146,3 +146,46 @@ public class SalaryOfMonth {
         }
 }
 
+package com.useCase5;
+
+import java.util.Random;
+
+
+
+public class EmpWage {
+
+        public static void main(String[] args) {
+                System.out.println("Welcome to employee wage computation");
+                int maxHrsInMonth=100;
+                int noOfWorkDay=20;
+                int empRatePerHr=20;
+                int isPartTime=1;
+                int isFullTime=2;
+                int empHrs=0;
+                int totalEmpHrs=0;
+                int totalWorkDay=0;
+                int totalEmpWage=0;
+        while( totalWorkDay<noOfWorkDay && totalEmpHrs<=maxHrsInMonth)
+        {
+                 Random random=new Random();
+                int randomNum=random.nextInt(3);
+                System.out.println("Random number: "+randomNum);
+        switch(randomNum)
+        {
+        case isPartTime: empHrs=4;
+                break;
+        case isFullTime: empHrs=8;
+                break;
+        default: empHrs=0;
+
+        }
+                totalWorkDay++;
+                totalEmpHrs=totalEmpHrs + empHrs;
+                System.out.println("employee days: "+totalWorkDay + "work hours: "+totalEmpHrs);
+totalEmpWage=(totalEmpHrs*empRatePerHr);
+System.out.println("employee total salary is: "+totalEmpWage);
+
+        }
+        }
+        }
+
