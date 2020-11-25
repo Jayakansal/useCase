f37674f6f302905f25598ee4b7c2d1990ea3249c
Jayakansal <21jayajindal@gmail.com>
2020-11-25 16:35:24 +0530

    This is wages of a month

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -104,3 +104,45 @@ public class WorkingHour {
 
         }
         }
+package com.useCase4;
+
+import java.util.Random;
+
+public class SalaryOfMonth {
+
+        public static void main(String[] args) {
+                int partTimeHours=4;
+                int fullTimeHours=8;
+                int FullTime=8;
+                int EmpRatePerHr=20;
+        double Salary;
+        int absent=0;
+       int WorkingDays=20;
+       int day;
+       int EmpHur;
+
+
+      for ( day= 1; day< WorkingDays; day++) {
+
+           Random random=new Random();
+           int randomNum=random.nextInt(3);
+
+           switch (randomNum) {
+                case 1:
+                        int partTimeWage=(partTimeHours*EmpRatePerHr);
+                        System.out.println("Part Time Employee " +partTimeWage);
+                        break;
+                case 2:
+                        int fullTimeWage=(fullTimeHours*EmpRatePerHr);
+                        System.out.println("Full Time Employee " +fullTimeWage);
+                        break;
+
+                default:
+                        System.out.println("Absent"+ absent);
+                        break;
+                }
+
+}
+        }
+}
+
