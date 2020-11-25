ffd0bbba3bed221d0c2fd53596cbf596cab3d2b4
Jayakansal <21jayajindal@gmail.com>
2020-11-25 16:29:50 +0530

    This is Salary according to part/full time

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -68,3 +68,39 @@ public class PartTimeORFullTime {
 
 
         }
+package com.useCase3;
+
+import java.util.Random;
+
+public class WorkingHour {
+
+
+        public static void main(String[] args) {
+                int PartTime=1;
+                int FullTime=2;
+                int EmpRatePerHr=20;
+                double Salary;
+                int absent=0;
+                int partTimeHour=4;
+                int fullTimeHour=8;
+                Random random=new Random();
+                int randomNum=random.nextInt(3);
+                int EmpHur;
+                switch (randomNum) {
+                case 1:
+                        int partTimeWage=(partTimeHour*EmpRatePerHr);
+                        System.out.println("Part Time Employee " +partTimeWage);
+                        break;
+                case 2:
+                        int fullTimeWage=(fullTimeHour*EmpRatePerHr);
+                        System.out.println("Full Time Employee " +fullTimeWage);
+                        break;
+
+                default:
+                        System.out.println("Absent"+ absent);
+                        break;
+                }
+
+
+        }
+        }
