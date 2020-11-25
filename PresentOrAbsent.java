2244c6a8138d8846c0928bc7895ec955552ad3fd
Jayakansal <21jayajindal@gmail.com>
2020-11-25 16:22:32 +0530

    This is for part or full time job

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -35,3 +35,36 @@ System.out.println("Salary:(int EmpHur* int ratePerHur)}");
 System.out.println("Employee is Absent");
 System.out.println("Salary:+0");
 }
+package com.useCase2;
+
+import java.util.Random;
+
+public class PartTimeORFullTime {
+
+        public static void main(String[] args) {
+                int PartTime=1;
+                int FullTime=2;
+                int EmpRatePerHr=20;
+                int EmpHur=0;
+
+                Random random=new Random();
+                int randomNum=random.nextInt(3);
+                if (randomNum==1)
+                {
+                 EmpHur=4;
+                }
+                else if (randomNum==2) {
+                 EmpHur=8;
+                }
+                else {
+                         EmpHur=0;
+                }
+
+                System.out.println("Employee Hours"+ EmpHur);
+                System.out.println("Salary:"+( EmpHur*EmpRatePerHr));
+
+
+                }
+
+
+        }
