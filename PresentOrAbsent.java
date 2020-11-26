f15386c59942bb962f5f6ebf2edabb430f2b3bfb
Jayakansal <21jayajindal@gmail.com>
2020-11-26 13:23:28 +0530

    Total Wage for each Company

--- PresentOrAbsent.java
+++ PresentOrAbsent.java
@@ -286,4 +286,50 @@ public class EmpWageBuilderMultiCompany {
 
 	}
 
-}
+package com.employee.usecases;
+
+import java.util.Random;
+
+public class EmpWageBuilderObject {
+
+        public static final int IS_PART_TIME=1;
+        public static final int IS_FULL_TIME=2;
+
+        private final String company;
+        private final int empRatePerHour;
+        private final int numOfWorkingDays;
+        private final int maxHoursPerMonth;
+
+        private int totalEmpWage;
+
+        public EmpWageBuilderObject(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+                super();
+                this.company = company;
+                this.empRatePerHour = empRatePerHour;
+                this.numOfWorkingDays = numOfWorkingDays;
+                this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+
+        public void computeEmpWage()
+        {
+        int empHrs=0,totalEmpHrs=0,totalWorkingDays=0;
+        Random random=new Random();
+        while(totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays)
+        {
+                totalWorkingDays++;
+
+                int empCheck=random.nextInt(3);
+                                //(int) Math.floor(Math.random() * 10)%3;
+                switch(empCheck)
+                {
+                case IS_PART_TIME:
+                        empHrs=4;
+                        break;
+
+                case IS_FULL_TIME:
+                        empHrs=8;
+                        break;
+                default:
+                        empHrs=0;
+
